e16316aa284b1dc63d6a9c439af277f674f7e74c
Ashish Nagar <ashishnagar033@gmail.com>
2026-06-29 09:45:16 +0530

    morgan added

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,22 @@ import express, { Express } from "express"
 import { errorHandler } from "./middlewares/errorHandler.middleware"
 import { routeNotFound } from "./middlewares/route-not-found"
 import v1Router from "./routers/v1"
+import morgan from "morgan"
+import { NODE_ENV } from "./config/env"
 
 const app: Express = express()
 
 app.use(express.json())
 app.use(express.urlencoded())
 
+// morgan
+app.use(morgan(NODE_ENV === 'production' ? "combined" : "dev"))
+
 
 // routes
 app.use('/api/v1', v1Router)
 
-
+// health route
 app.get('/health', (_req ,res) => {
     console.log("Execute health route")
 
