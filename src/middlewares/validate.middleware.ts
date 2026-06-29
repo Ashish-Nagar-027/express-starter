cca9103bd12200cc29fa4c81aa13014b03f90926
Ashish Nagar <ashishnagar033@gmail.com>
2026-06-29 10:18:32 +0530

    zod validations update

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -2,7 +2,14 @@ import type { Request, Response, NextFunction } from "express";
 import {z} from 'zod'
 import { badRequest } from "../utils/api-error";
 
-export function validateRequiestBody(schema:z.ZodSchema) {
+
+/**
+ * 
+ * @param schema - Zod schema to validate the request body
+ * @returns - Middleware function to validate the request body
+ */
+
+export function validateRequiestBody(schema: z.ZodSchema) {
   return (req:Request, _res:Response, next:NextFunction) => {
     const result = schema.safeParse(req.body)
 
@@ -15,3 +22,42 @@ export function validateRequiestBody(schema:z.ZodSchema) {
     next()
   }
 }
+
+/**
+ * 
+ * @param schema - Zod schema to validate the req query params
+ * @returns - Middleware function to validate the request query params
+ */
+export function validateQueryParams(schema: z.ZodSchema) {
+  return (req:Request, _res:Response, next:NextFunction) => {
+    const result = schema.safeParse(req.query)
+
+    if(!result.success){
+        throw badRequest("Validation failed", result.error.issues)
+    }
+
+    req.body = result.data
+
+    next()
+  }
+}
+
+/**
+ * 
+ * @param schema - Zod schema to validate the request params
+ * @returns - Middleware function to validate the request params
+ */
+export function validateRequestParams(schema: z.ZodSchema) {
+  return (req:Request, _res:Response, next:NextFunction) => {
+    const result = schema.safeParse(req.params)
+
+    if(!result.success){
+        throw badRequest("Validation failed", result.error.issues)
+    }
+
+    req.body = result.data
+
+    next()
+  }
+}
+
